1534df154bedb4c60cf9f21df58423aeb42a36ee
Daniel Hashmi <danialhashmi418@gmail.com>
2025-12-01 20:51:53 +0500

    008: Integrate ContentHighlight component into main layout

--- docs/src/theme/Layout/index.tsx
+++ docs/src/theme/Layout/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from '@theme-original/Layout';
 import { AiChatButton } from '@site/src/components/Chat';
 import { AuthProvider } from '@site/src/components/AuthContext';
+import ContentHighlight from '@site/src/components/ContentHighlight';
 import type {Props} from '@theme/Layout';
 
 export default function LayoutWrapper(props: Props): JSX.Element {
@@ -9,6 +10,7 @@ export default function LayoutWrapper(props: Props): JSX.Element {
     <AuthProvider>
       <Layout {...props} />
       <AiChatButton />
+      <ContentHighlight />
     </AuthProvider>
   );
 }
